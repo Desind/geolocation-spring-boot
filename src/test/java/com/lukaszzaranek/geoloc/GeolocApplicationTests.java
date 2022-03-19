b53fdc46d5b64c86a7f5d9f749be9859fb402584
Desind <zaran1998@gmail.com>
2022-03-19 11:55:40 +0100

    initial

---
+++ src/test/java/com/lukaszzaranek/geoloc/GeolocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukaszzaranek.geoloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
